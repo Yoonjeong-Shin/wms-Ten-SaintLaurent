24feccad6b39bb363154c2a9628a3c4b285d2260
kishi <81032378+vlshzl35@users.noreply.github.com>
2024-05-16 18:02:28 +0900

    Update: inbound controller/mapper 틀 작성

--- whsService/src/main/java/com/sh/view/InboundView.java
+++ whsService/src/main/java/com/sh/view/InboundView.java
@@ -1,4 +1,19 @@
 package com.sh.view;
 
+import com.sh.controller.InboundController;
+
+import java.util.Scanner;
+
 public class InboundView {
+
+    private InboundController inboundController = new InboundController();
+    private Scanner sc = new Scanner(System.in);
+
+    // 제조업체가 창고에게 입고승인 메소드를 호출한다.
+
+    // 요청이 왔다치고 구현하기
+
+
+
+
 }
