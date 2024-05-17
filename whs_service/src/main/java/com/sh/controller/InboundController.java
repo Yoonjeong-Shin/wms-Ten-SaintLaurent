ac03fe0fc8df7ffe8ea644ef232a15ee28008704
jyna120 <jyna120@naver.com>
2024-05-17 23:20:58 +0900

    Feat: Inbound 입고 조회 구현

--- whs_service/src/main/java/com/sh/controller/InboundController.java
+++ whs_service/src/main/java/com/sh/controller/InboundController.java
@@ -2,22 +2,15 @@ package com.sh.controller;
 
 import com.sh.model.dto.InboundDto;
 import com.sh.model.service.InboundService;
-
-import java.util.List;
+import com.sh.view.InboundResultView;
 
 public class InboundController {
     private InboundService inboundService = new InboundService();
 
-    // 입고 정보 조회 (입고 승인과 입고 확정 때 쓰인다)
-    // INB_TB의 한 데이터를 INB_ID_PK로 조회
-    public void findByInboundID(int inboundID) {
-        inboundService.findByInboundID(inboundID);
-    }
-
     // 입고 승인
     // LOCATE_TB에서 LOCATE_ITEM_CNT 찾기
     public void findLocateItemCnt() {
-        inboundService.findLocateItemCnt();
+        inboundService.findEmptyLocate();
     }
 
     // 입고 승인
@@ -26,15 +19,36 @@ public class InboundController {
         inboundService.insertInboundToINB(inboundDto);
     }
 
+    /* 지영 작업 시작 */
+
+    // 입고 정보 조회 (입고 승인과 입고 확정 때 쓰인다)
+    // INB_TB의 한 데이터의 모든 정보를 INB_ID_PK로 조회
+    public void findByInbId(int inbIdPk) {
+        try {
+            // 0~1개의 MenuDto 반환 : MenuDto
+            InboundDto inboundDto = inboundService.findByInbId(inbIdPk);
+            InboundResultView.displayInb(inboundDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        inboundService.findByInbId(inbIdPk);
+    }
+
     // 입고 검수
     // GBG_TB에 state가 2,3인 불량 제품을 insert
-    public void insertInboundToGBG(InboundDto inboundDto){
-        inboundService.insertInboundToGBG(inboundDto);
+    public void insertInbToGbg(InboundDto inboundDto){
+        inboundService.insertInbToGbg(inboundDto);
+    }
+    // GBG_DETAIL_TB에 state가 2,3인 불량 제품을 insert
+    public void insertInbToGbgDetail(InboundDto inboundDto){
+        inboundService.insertInbToGbgDetail(inboundDto);
     }
 
     // 입고 검수
     // INB_TB에서 불량 제품을 뺀 수량 update
-    public void updateInboundCnt(InboundDto inboundDto){
-        inboundService.updateInboundCnt(inboundDto);
+    public void updateInbCnt(InboundDto inboundDto){
+        inboundService.updateInbCnt(inboundDto);
     }
+
+    /* 지영 작업 끝 */
 }
